f4b308f71497be609735a6dcfca43fbd8e2e4d86
Darin Hensley <darin.hensley@gm.com>
2017-12-29 17:12:41 -0600

    na

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef
+} from '@angular/core';
+
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 
 // LEAVE OFF ... ADD BREAK POINT OBSERVER
-export class AppComponent {
-   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-      this.mobileQuery = media.matchMedia('(max-width: 600px)');
+export class AppComponent implements AfterViewInit {
+   @ViewChild('snav') public snav: MdSidenav;
+
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: (media) => void;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher) {
+      this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
+  }
+
+  ngAfterViewInit() { 
+    this._mobileQueryListener = (media) => { 
+      if (media.matches && this.snav.opened) { 
+        this.snav.close();
+      } else if (!media.matches && !this.snav.opened) {
+        this.snav.open();
+      }
+      this.changeDetectorRef.detectChanges();
+    };
+
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 }
